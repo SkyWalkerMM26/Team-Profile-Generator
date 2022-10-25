0a4fc813fb9aa34392c9d6ab51540cbdb925ff29
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-10-25 08:39:25 -0700

    duplicate the card for engineer and intern in src page-template

--- src/page-template.js
+++ src/page-template.js
@@ -18,6 +18,40 @@ const team = (crew) => {
     </div>
         `;
     };
+    const createEngineer = (engineer) => {
+        return `
+        <div class="card team-member-card">
+        <div class="card-header">
+            <h2 class="card-title"></i>${engineer.role()}</h2>
+            <h3 class="card-title">${engineer.name()}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="data-list-info">ID: ${engineer.id()}</li>
+                <li class="data-list-info">Email: <a href="mailto:${engineer.email()}">${engineer.email()}</a></li>
+                <li class="data-list-info">Office number: ${engineer.officeNumber()}</li>
+            </ul>
+        </div>
+    </div>
+        `;
+    };
+    const createIntern = (intern) => {
+        return `
+        <div class="card team-member-card">
+        <div class="card-header">
+            <h2 class="card-title"></i>${intern.role()}</h2>
+            <h3 class="card-title">${intern.name()}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="data-list-info">ID: ${intern.id()}</li>
+                <li class="data-list-info">Email: <a href="mailto:${intern.email()}">${intern.email()}</a></li>
+                <li class="data-list-info">Office number: ${intern.officeNumber()}</li>
+            </ul>
+        </div>
+    </div>
+        `;
+    };
 
 
 
