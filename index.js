fef236d6a6873a463ed8d17605675f21e62f5381
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-10-25 07:19:53 -0700

    change the quetions to be async and require the engineer, intern, and manager files to index.js

--- index.js
+++ index.js
@@ -1,49 +1,41 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateHTML = (answers) =>
-  `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
 
-</body>
-</html>`;
-
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: "What is the team manager's name?",
-    },
-    {
-      type: 'input',
-      name: 'id',
-      message: "What is the team manager's id?",
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: "What is the team manager's email?",
-    },
-    {
-      type: 'input',
-      name: 'number',
-      message: "What is the team manager's office number?",
-    },
-    {
-      type: 'list',
-      name: 'teammate add',
-      message: "Which type of the team member would you like to add?",
-      choices: ["Manager","Engineer","Intern","I don't want to add any more team members."],
-    },
-  ])
+// These are the questions for the prompt
+const questions = async () => {
+  const answers = await inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: "What is the team manager's name?",
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: "What is the team manager's id?",
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: "What is the team manager's email?",
+      },
+      {
+        type: 'input',
+        name: 'number',
+        message: "What is the team manager's office number?",
+      },
+      {
+        type: 'list',
+        name: 'teammate add',
+        message: "Which type of the team member would you like to add?",
+        choices: ["Manager","Engineer","Intern","I don't want to add any more team members."],
+      },
+    ])
   .then((answers) => {
     const htmlPageContent = generateHTML(answers);
 
