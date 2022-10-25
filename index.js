ecf7425a05bad5672c172db27667d155dbdbd92a
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-10-25 06:22:35 -0700

    Create inquirer prompt

---
+++ index.js
@@ -0,0 +1,52 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generateHTML = (answers) =>
+  `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>`;
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: "What is the team manager's name?",
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: "What is the team manager's id?",
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: "What is the team's manager email?",
+    },
+    {
+      type: 'input',
+      name: 'number',
+      message: "What is the team manager's office number?",
+    },
+    {
+      type: 'input',
+      name: 'teammate add',
+      message: "Which type of the team member would you like to add?",
+    },
+  ])
+  .then((answers) => {
+    const htmlPageContent = generateHTML(answers);
+
+    fs.writeFile('./dist/team.html', htmlPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created team.html!')
+    );
+  });
