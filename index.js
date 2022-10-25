7eebd2b71fd5e7a9848ef74802fb220b835fed8c
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-10-25 07:43:08 -0700

    create an if fxn if the user didn't select don wan't to create new member to prompt the questions again

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+// These are library modules.
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
-// These are the questions for the prompt
+// The answers of the questions will go in here.
+const staffInformation = [];
+
+// These are the questions for the prompt.
 const questions = async () => {
   const answers = await inquirer
     .prompt([
@@ -29,17 +33,25 @@ const questions = async () => {
         name: 'number',
         message: "What is the team manager's office number?",
       },
-      {
-        type: 'list',
-        name: 'teammate add',
-        message: "Which type of the team member would you like to add?",
-        choices: ["Manager","Engineer","Intern","I don't want to add any more team members."],
-      },
     ])
-  .then((answers) => {
-    const htmlPageContent = generateHTML(answers);
+}
 
-    fs.writeFile('team.html', htmlPageContent, (err) =>
-      err ? console.log(err) : console.log('Successfully created team.html!')
-    );
-  });
+    async function addNewMember(){
+      await questions()
+
+      const teamAddAns = await inquirer
+        .prompt([
+          {
+            type: 'list',
+            name: 'teamAdd',
+            message: "Which type of the team member would you like to add?",
+            choices: ["Manager","Engineer","Intern","I don't want to add any more team members."],
+          },
+        ])
+
+      if (teamAddAns.teamAdd === "I don't want to add any more team members."){
+        return addNewMember()
+      }
+      return generateTeam();
+}
+
