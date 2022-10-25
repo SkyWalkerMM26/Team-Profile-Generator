8d1e58cf13d7096d67951e1a2ecc13c3e0b9372b
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-10-25 08:10:56 -0700

    create if statements to store users' information and push it to an empty variable to pass it onto a html

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateTeam = require("./src/page-template.js");
 
 // These are library modules.
 const Engineer = require("./lib/Engineer");
@@ -16,42 +17,89 @@ const questions = async () => {
       {
         type: 'input',
         name: 'name',
-        message: "What is the team manager's name?",
+        message: "What is the your name?",
       },
       {
         type: 'input',
         name: 'id',
-        message: "What is the team manager's id?",
+        message: "What is the your id?",
       },
       {
         type: 'input',
         name: 'email',
-        message: "What is the team manager's email?",
+        message: "What is the your email?",
       },
       {
         type: 'input',
         name: 'number',
-        message: "What is the team manager's office number?",
+        message: "What is the team your office number?",
+      },
+      {
+        type: 'list',
+        name: 'role',
+        message: "What is the team your role?",
+        choices: ['Manager','Engineer','Intern'],
       },
     ])
+
+    // Manager answers
+    if (answers.role === "Manager") {
+        const newManager = new Manager(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.officeNumber,
+          answers.role
+        );
+        staffInformation.push(newManager);
+        
+      // Engineer answers
+    } else if (answers.role === "Engineer") {
+        const newEngineer = new Engineer(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.officeNumber,
+          answers.role
+        );
+        staffInformation.push(newEngineer);
+        
+      // Intern Answers
+    } else if (answers.role === "Intern") {
+        const newIntern = new Intern(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.officeNumber,
+          answers.role
+        );
+        staffInformation.push(newIntern);     
+      } 
 }
 
-    async function addNewMember(){
-      await questions()
+// Listens if user wants to add more members.
+async function addNewMember(){
+  await questions()
 
-      const teamAddAns = await inquirer
-        .prompt([
-          {
-            type: 'list',
-            name: 'teamAdd',
-            message: "Which type of the team member would you like to add?",
-            choices: ["Manager","Engineer","Intern","I don't want to add any more team members."],
-          },
-        ])
+  const teamAddAns = await inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'teamAdd',
+        message: "Which type of the team member would you like to add?",
+        choices: ["Manager","Engineer","Intern","I don't want to add any more team members."],
+      },
+    ])
 
-      if (teamAddAns.teamAdd === "I don't want to add any more team members."){
-        return addNewMember()
-      }
-      return generateTeam();
+  if (teamAddAns.teamAdd !== "I don't want to add any more team members."){
+    return addNewMember()
+  }
+  return generateTeam();
 }
 
+addNewMember();
+
+// create html file with the answers
+function generateTeam(){
+  fs.writeFileSync("./dist/team.html", htmlTeam(staffInformation));
+}
