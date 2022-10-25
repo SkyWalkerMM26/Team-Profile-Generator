5d0398b95ab330a54072f96547a3d79e436937c6
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-10-25 08:30:13 -0700

    add css style to page-template.js

--- src/page-template.js
+++ src/page-template.js
@@ -3,16 +3,16 @@
 const team = (crew) => {
     const createManager = (manager) => {
         return `
-        <div class="card employee-card">
+        <div class="card team-member-card">
         <div class="card-header">
-            <h2 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.role()}</h2>
+            <h2 class="card-title"></i>${manager.role()}</h2>
             <h3 class="card-title">${manager.name()}</h3>
         </div>
         <div class="card-body">
             <ul class="list-group">
-                <li class="list-group-item">ID: ${manager.id()}</li>
-                <li class="list-group-item">Email: <a href="mailto:${manager.email()}">${manager.email()}</a></li>
-                <li class="list-group-item">Office number: ${manager.officeNumber()}</li>
+                <li class="data-list-info">ID: ${manager.id()}</li>
+                <li class="data-list-info">Email: <a href="mailto:${manager.email()}">${manager.email()}</a></li>
+                <li class="data-list-info">Office number: ${manager.officeNumber()}</li>
             </ul>
         </div>
     </div>
